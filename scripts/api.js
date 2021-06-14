61203e206a68d3657140df1d648f0756082df952
Sr_santi <santiriosolaya@gmail.com>
2021-06-14 00:07:53 -0500

    need solve modal funcionality in trending

--- scripts/api.js
+++ scripts/api.js
@@ -3,7 +3,11 @@ let carousel_trending_container= document.querySelector('.carousel')
 APIKEY= "DCAxXJyKXP7rMfWdncYE04ImQn07Cvfg"
 
 function init_trending() {
-    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=25`
+    /**
+     * This function generates de request to api in trending endpoint
+     * and show 25 most populars gifs in carousel
+     */
+    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=10`
     let gif_html_element = ''
     console.log(url)
     fetch(url)
@@ -16,19 +20,22 @@ function init_trending() {
         for (let index_gif = 0; index_gif < content.data.length; index_gif++) {
             //
             gif_html_element += `
-            <div class="carousel-item">
-                <img class="carousel-item__img" src="${content.data[index_gif].images.downsized.url}" alt="${content.data[index_gif].title}">
-                <div class="carousel-item__details">
+            <div class="bg-modal">
+            <span class="bg-modal__modal-close"><i class="fas fa-times"></i></span>
+                <div class="carousel-item">
+                    <img class="carousel-item__img" src="${content.data[index_gif].images.downsized.url}" alt="${content.data[index_gif].title}">
+                    <div class="carousel-item__details">
 
-                    <div class="carousel-item__buttons">
-                        <img id="favorite" src="/assets/icon-fav.svg" alt="favorite_icon">
-                        <img id="download" src="/assets/icon-download.svg" alt="favorite_icon">
-                        <img id="max" src="/assets/icon-max-normal.svg" alt="favorite_icon">
-                    </div>
+                        <div class="carousel-item__buttons">
+                            <img id="favorite" src="/assets/icon-fav.svg" alt="favorite_icon">
+                            <img id="download" src="/assets/icon-download.svg" alt="favorite_icon">
+                            <img id="max" src="/assets/icon-max-normal.svg" alt="favorite_icon">
+                        </div>
 
-                    <div class="carousel-item__info">
-                        <p>${content.data[index_gif].username}</p>
-                        <h3>${content.data[index_gif].title}</h3>
+                        <div class="carousel-item__info">
+                            <p>${content.data[index_gif].username}</p>
+                            <h3>${content.data[index_gif].title}</h3>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -36,6 +43,38 @@ function init_trending() {
         }
         //----------------------------
         carousel_trending_container.innerHTML += gif_html_element
+        bg_modal_items = carousel.querySelectorAll(':scope > div')
+        console.log(bg_modal_items)
+
+        bg_modal_items.forEach(
+            function (modal_item) {
+                const carousel_item = modal_item.querySelector('.carousel-item')
+                const modal_close = modal_item.getElementsByClassName('bg-modal__modal-close')
+                modal_close.hidden= true
+                console.log(modal_close)
+                modal_item.addEventListener("click", ()=> {
+                    modal_item.classList.remove("bg-modal")
+                    modal_item.classList.add("bg-modal-active")
+                    modal_close.hidden=false
+                })
+                
+                if(modal_close){
+
+                    modal_close.addEventListener("click", ()=> {
+                        modal_item.classList.remove("bg-modal-active")
+                        modal_item.classList.add("bg-modal")
+                        modal_close.hidden=true
+                    })
+                }
+                /* window.onclick = function(event) {
+                    if (event.target == modal_item) {
+                    modal_item.classList.remove("bg-modal-active")
+                    modal_item.classList.add("bg-modal")
+                    }
+                } */
+
+            }
+          )
     })
     .catch(err => {
         console.error(err)
