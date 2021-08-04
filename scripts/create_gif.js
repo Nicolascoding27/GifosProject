917e1f0b98a4df968d6f3bd7672404bfb632cb2b
Sr_santi <santiriosolaya@gmail.com>
2021-08-04 14:28:13 -0500

    views in show-container created, need to apply funcionalities 😫😛

---
+++ scripts/create_gif.js
@@ -0,0 +1,39 @@
+/* vista 1
+<div id="show_container" class="record__container--display_messague">
+    <h2>¿Nos das acceso</h2>
+    <h2>a tu cámara?</h2>
+    <p>El acceso a tu camara será válido sólo</p>
+    <p>por el tiempo en el que estés creando el GIFO.</p>
+</div>
+*/
+
+/* vista 2
+<div id="show_container" class="record__container--display_messague">
+    <h2>¿Nos das acceso</h2>
+    <h2>a tu cámara?</h2>
+    <p>El acceso a tu camara será válido sólo</p>
+    <p>por el tiempo en el que estés creando el GIFO.</p>
+</div>
+*/
+
+/* vista 3
+<div id="show_container" class="record__container--display_cover">
+    <div class="record__container--messague_check alself-center">
+    <img id="loader" src="/assets/loader.svg" alt="loader">
+    <p class="record__container--messague_check-text">Estamos subiendo tu GIFO</p>
+    </div>
+</div>
+*/
+
+/* vista 4
+<div id="show_container" class="record__container--display_cover">
+    <div class="record__container--options">
+    <div id="download-record" aria-labelledby="download-record"><a href="#" download></a></div>
+    <div id="link-record" class="link-record" aria-labelledby="delete_icon"></div>
+    </div>
+    <div class="record__container--messague_check">
+    <img src="/assets/check.svg" alt="check">
+    <p class="record__container--messague_check-text">GIFO subido con éxito</p>
+    </div>
+</div>
+*/
